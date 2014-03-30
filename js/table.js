8b1430acede01efdd2a7311b2f1f29e2efd712a3
Mu-An Chiou <me@muanchiou.com>
2014-03-29 23:07:16 -0700

    Change past event logic

--- js/table.js
+++ js/table.js
@@ -38,7 +38,6 @@ function addMonthMenu() {
 }
 
 function appendEvent( event ) {
-  today          = new Date()
   eventStartDate = new Date(event.startdate)
   eventEndDate   = new Date(event.enddate)
   eventElement   = $('<div class="event"><a target="_blank" href="' + event.tickets + '">' + event.name + '</a></div>')
@@ -69,7 +68,6 @@ function appendEvent( event ) {
     }
   }
 
-  if(today > eventStartDate){ eventElement.addClass('past-event') }
   $('#' + formattedDate(eventStartDate)).removeClass('no-event').append(eventElement)
 }
 
@@ -90,6 +88,8 @@ function generateMonthTable( date ) {
   eventMonthName = monthNames[date.getMonth()]
   monthTable     = $('<table cellspacing=0 class="month-table" data-month="' + eventMonthName + '" id="month-' + date.getMonth() + '"></table>')
   monthTableBody = monthTable.append('<tbody>')
+  today          = new Date()
+  endOfToday     = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 00, 00, 00)
   firstDay       = new Date(date.getFullYear(), date.getMonth(), 1)
   numberOfDays   = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
   weekDayNumber  = firstDay.getDay()
@@ -113,7 +113,8 @@ function generateMonthTable( date ) {
   loopForTimes( numberOfDays, function(daynumber) {
     thisDay = new Date(date.getFullYear(), date.getMonth(), (daynumber + 1))
     id = formattedDate(thisDay)
-    getFirstAvailableRow(monthTableBody).append('<td class="no-event" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>')
+    pastClass = endOfToday > thisDay ? "past" : ""
+    getFirstAvailableRow(monthTableBody).append('<td class="no-event ' + pastClass + '" id=' + id + '><div class=day>'+ (daynumber + 1) +'</div></td>')
   })
 
   // Add empty days from next month
