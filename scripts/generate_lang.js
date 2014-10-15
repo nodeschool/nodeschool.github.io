b6b5b9527ca0240604140e83455c8265bac142ae
Thomas Watson Steen <w@tson.dk>
2014-10-15 14:49:11 +0800

    Extract cheerio eq finder to its own module

--- scripts/generate_lang.js
+++ scripts/generate_lang.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var cheerio = require('cheerio');
+var find = require('cheerio-eq');
 var afterAll = require('after-all');
 var selectors = require('../languages/selectors.json');
 
@@ -28,32 +29,12 @@ fs.readdir(path.join(__dirname, '..'), function (err, files) {
   });
 });
 
-// cheerio doesn't care much for :eq() selectors, let's take care of that
-var cheeriofySelector = function ($, selector) {
-  var regex = /^(.*?)(?:\:eq\((\d+)\))(.*)/;
-  var parts = [];
-  var match;
-  while (match = selector.match(regex)) {
-    parts.push(match[1]);
-    parts.push(parseInt(match[2], 10));
-    selector = match[3];
-  }
-  parts.push(selector);
-
-  var cursor = $(parts.shift());
-  parts.forEach(function (selector) {
-    cursor = typeof selector === 'number' ? cursor.eq(selector) : cursor.find(selector);
-  });
-
-  return cursor;
-};
-
 var processFile = function (lang, filename, callback) {
   fs.readFile(filename, function (err, data) {
     if (err) return callback(err);
     var $ = cheerio.load(data);
     Object.keys(selectors).forEach(function (selector) {
-      var text = cheeriofySelector($, selector).text().trim();
+      var text = find($, selector).text().trim();
       var key = selectors[selector];
       if (!(key in lang)) lang[key] = null;
       if (!text) return; // maybe this selector doesn't match the current file
