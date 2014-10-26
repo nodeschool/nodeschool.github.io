7b85b500cb7b7bcba000a0f169d060d3a53b3aad
Robert Kowalski <rok@kowalski.gd>
2014-10-26 17:28:44 +0100

    Remove duplicate code
    The function `makeMap` in `index.js` is the same, except a patch
    that was just applied to this function in
    3e947bf4fdb6d6adbd399383234c34d4e473f425 which now also applies to
    the function that was in events.html

--- js/index.js
+++ js/index.js
@@ -25,35 +25,6 @@ function showNearEvents(data) {
   }
 }
 
-function makeMap(data) {
-  data = addHexcolor(data, "#F7DA03", "#A09C9C")
-  // make map
-  var optionsJSON = ["name", "tickets", "startdate", "state"]
-  var template = "<p class='event'>{{startdate}} <a class='{{state}}' href='{{tickets}}'"
-    + " target='_blank'>{{name}}</a><p>"
-  var geoJSON = Sheetsee.createGeoJSON(data, optionsJSON)
-  var map = Sheetsee.loadMap("map")
-  Sheetsee.addTileLayer(map, 'examples.map-20v6611k')
-  var markerLayer = Sheetsee.addMarkerLayer(geoJSON, map, template)
-  map.setZoom(1)
-  // disable dragging on mobile/tablet because it makes it hard
-  // to scroll the page on full width-ish maps
-  if (window.innerWidth <= 768 ) map.dragging.disable()
-}
-
-function addHexcolor(data, color, color2) {
-  data.forEach(function(event) {
-    event.state = "past"
-    event.startUTC = new Date(event.startdate)
-    if (event.startUTC > new Date()) {
-      event.hexcolor = color
-      event.state = "future"
-    }
-    else event.hecolor = color2
-  })
-  return data
-}
-
 function writeCount(count) {
   $('#event-count .cnt').text(count)
 }
