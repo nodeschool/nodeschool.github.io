88c10c47a611736553533b4e1c2ea02be7a5615e
sudodoki <smd.deluzion@gmail.com>
2015-05-19 23:56:59 -0400

    Add google analytics tracking for workshopper links clicks

--- js/index.js
+++ js/index.js
@@ -28,3 +28,13 @@ function showNearEvents(data) {
 function writeCount(count) {
   $('#event-count .cnt').text(count)
 }
+
+// init google analytics tracking events for clicks on workshop external links
+$('#workshopper-list, .elective-workshoppers')
+  // assuming current layout (first .third used for info)
+  .find('.third:not(:eq(0))')
+  .on('click', 'a', function (e) {
+    // assuming github.com/username/workshopper
+    var workshopperId = $(this).attr('href').split('/').slice(-1)[0];
+    ga('send', 'event', 'workshopper', 'click', workshopperId);
+  });
