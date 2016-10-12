8d9f179d9d39d512695407a935f6fd3b423a9429
JP Wesselink <jp@The-Lotus-Eater.local>
2016-10-12 10:23:54 +0200

    Adds community groups

--- scripts/generate-css.sh
+++ scripts/generate-css.sh
@@ -5,6 +5,7 @@
 #   -u = utilize plugin
 
 stylus -u autoprefixer-stylus -c -m ./styles/chapters.styl -o ./chapters.css
+stylus -u autoprefixer-stylus -c -m ./styles/community-groups.styl -o ./community-groups.css
 stylus -u autoprefixer-stylus -c -m ./styles/events.styl -o ./events.css
 stylus -u autoprefixer-stylus -c -m ./styles/mapbox.styl -o ./mapbox.css
 stylus -u autoprefixer-stylus -c -m ./styles/style.styl -o ./style.css
