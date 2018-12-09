2e880d1185a295bb0f7faf4af5c628886711aa1c
Marc Picaud <picaud.marc@gmail.com>
2018-12-09 23:09:24 +0100

    🔥 Removed .env check

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-if [[ -f ../.env ]]
-then
-	echo 'Missing .env file'
-	echo 'Please consult README for setup instructions'
-	exit 1
-fi
 npm run build-chapters -- $@
 npm run generate-css
 npm run build-dependencies
