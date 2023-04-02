776edb3e7ff74d24cb9b96a4d073d101f147138d
Finn Pauls <mbp@pauls.dev>
2023-04-02 22:32:12 +0200

    Remove broken list of events

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -14,14 +14,10 @@ if (env.error) {
 	throw env.error;
 }
 
-if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
-	throw new Error('GITHUB_PERSONAL_ACCESS_TOKEN environment variable is not found');
-}
+// if (!process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
+// 	throw new Error('GITHUB_PERSONAL_ACCESS_TOKEN environment variable is not found');
+// }
 
-const axios = require('axios').create({
-	baseURL: 'https://api.github.com/',
-	headers: { 'Authorization': `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}` }
-});
 
 function createLangButton(dom, file, lang, langName) {
 var li = dom.createElement('li')
@@ -86,28 +82,6 @@ function getNodeByLanguage(nodes, lang) {
 	}
 }
 
-async function getStargazers(workshopElement) {
-	const url = require('url');
-	const githubUrl = workshopElement.getAttribute("href");
-	let response, parsedUrl;
-
-	if (!githubUrl) {
-		return 0;
-	}
-
-	try {
-		parsedUrl = url.parse(githubUrl);
-	} catch (e) {
-		throw e;
-	}
-
-	try {
-		response = await axios.get(`/repos${parsedUrl.path}?per_page=1`);
-	} catch (e) {
-		throw e;
-	}
-	writeStargazers(workshopElement, response.data.stargazers_count)
-}
 
 function writeStargazers(workshopElement, stargazersCount) {
 	const starIconSVG = `<svg style="vertical-align: middle" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill="currentColor" fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>`;
@@ -172,15 +146,6 @@ cmdwatcher('build-html'
 		}
 		try {
 			dom = jsdom.jsdom(raw)
-			if (file === 'index.html') {
-				const t0 = performance.now();
-				const workshops = dom.getElementsByClassName('js-workshop-link');
-				await isRateLimitReached(workshops.length);
-				const githubPromises = Array.from(workshops).map(getStargazers);
-				await Promise.all(githubPromises);
-				const t1 = performance.now();
-				console.log(`⭐️ GitHub Stargazers fetched in ${((t1 - t0) / 1000).toFixed(1)}s`)
-			}
 		} catch(e) {
 			return console.log('Error while domify %s:\n%s', file, e)
 		}
